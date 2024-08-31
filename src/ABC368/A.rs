379bcb14dc29d70f48287f3a2e5975826e9754ba
nokonoko1203 <healtheworld.1203@gmail.com>
2024-08-31 22:32:51 +0900

    a

--- src/ABC368/A.rs
+++ src/ABC368/A.rs
@@ -1,1 +1,24 @@
 // https://atcoder.jp/contests/abc368/tasks/abc368_a
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+    let result: Vec<usize> = a[n - k..]
+        .iter()
+        .chain(a[..n - k].iter())
+        .cloned()
+        .collect();
+
+    println!(
+        "{}",
+        result
+            .iter()
+            .map(|&x| x.to_string())
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize
+        n: usize,
+        k: usize,
+        a: [usize; n],
     }
+    let result: Vec<usize> = a[n - k..]
+        .iter()
+        .chain(a[..n - k].iter())
+        .cloned()
+        .collect();
+
+    println!(
+        "{}",
+        result
+            .iter()
+            .map(|&x| x.to_string())
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
 }
